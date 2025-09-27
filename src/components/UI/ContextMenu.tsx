32b506c70fc4d827d9e9ead5162b86fa7bb25b59
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-09-27 05:47:40 +0300

    close all tabs fuctionality

--- src/components/UI/ContextMenu.tsx
+++ src/components/UI/ContextMenu.tsx
@@ -1,26 +1,33 @@
 import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { setOpendFilesAction } from "../../app/features/fileTreeSlice";
 
 interface IProps {
-  setShowMenu:(val:boolean) => void;
+  setShowMenu: (val: boolean) => void;
   positions: {
     posX: number;
     posY: number;
   };
 }
-const ContextMenu = ({ positions,setShowMenu }: IProps) => {
+const ContextMenu = ({ positions, setShowMenu }: IProps) => {
+  const menuRef = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch();
 
-    const menuRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleClickOutSide = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event?.target as Node))
+        setShowMenu(false);
+    };
+    window.addEventListener("click", handleClickOutSide);
+    return () => {
+      window.removeEventListener("click", handleClickOutSide);
+    };
+  }, [setShowMenu]);
 
-    useEffect( () => {
-        const handleClickOutSide = (event:MouseEvent) =>{
-            if(menuRef.current && !menuRef.current.contains(event?.target as Node))
-                setShowMenu(false);
-        }
-        window.addEventListener("click",handleClickOutSide);
-        return () =>{
-            window.removeEventListener("click",handleClickOutSide);
-        }
-    }, [setShowMenu])
+  // **** Handlers
+  const closeAllHandler = () => {
+    dispatch(setOpendFilesAction([]));
+  };
 
   return (
     <div ref={menuRef}>
@@ -32,8 +39,8 @@ const ContextMenu = ({ positions,setShowMenu }: IProps) => {
           top: positions.posY,
         }}
       >
-        <li>Close </li>
-        <li> Close All </li>
+        <li className="cursor-pointer mt-2 hover:text-gray-400">Close </li>
+        <li onClick={closeAllHandler} className="cursor-pointer mt-2 hover:text-gray-400"> Close All </li>
       </ul>
     </div>
   );
